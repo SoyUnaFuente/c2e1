b0d0f6ea40ef55862487e7fdbd8fd61cadbee1bc
unknown <alexander.lafuente@correo.unimet.edu.ve>
2021-09-16 13:44:17 -0700

    Solved

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+
+
 volumen_reservorio = 4.445e8
 lluvia = 5e6
+
+reduced_lluvia = lluvia *.1
+
+
+new_volume = volumen_reservorio + reduced_lluvia
+
+new_volume_5 = new_volume * 1.05
+
+new_reduced_volume_2 = new_volume_5 * .02
+
+new_volumen_reservorio = new_reduced_volume_2 - 2.5e5
+
+print(f"El volumen del reservorio es {new_volumen_reservorio} m3")
 
